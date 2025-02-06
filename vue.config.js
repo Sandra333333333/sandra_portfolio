d7d7885da41ce659fdf7dd2e309579b90a259336
Sandra333333333 <sandra05213@gmail.com>
2025-02-06 21:24:00 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-}),
-{
-  publicPath: 'sandra_portfolio/dist/'
-}
+  transpileDependencies: true,
+  publicPath: '/sandra_portfolio/'
+})
